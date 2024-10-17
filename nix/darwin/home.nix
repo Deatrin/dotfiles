18853f14d2830c5a778644095cd0e8923d465669
Deatrin <jennexa@gmail.com>
2024-10-17 11:36:07 -0700

    Add nvim config

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -14,6 +14,8 @@ in {
   home.homeDirectory = "/Users/ajennex";
   xdg.enable = true;
 
+  xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/ajennex/Development/dotfiles/.config/nvim";
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
@@ -28,6 +30,7 @@ in {
     fzf = import ../home/fzf.nix {inherit pkgs;};
     git = import ../home/git.nix { inherit config pkgs; };
     oh-my-posh = import ../home/oh-my-posh.nix {inherit pkgs;};
+    neovim = import ../home/neovim.nix {inherit config pkgs;};
     tmux = import ../home/tmux.nix {inherit pkgs;};
     zsh = import ../home/zsh.nix {inherit config pkgs lib;};
   };
