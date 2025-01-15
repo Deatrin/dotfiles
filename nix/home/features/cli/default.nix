7a875360e8fdbdf7b41d641d259c93bcccff6630
Deatrin <jennexa@gmail.com>
2025-01-14 23:17:47 -0800

    start feature sets with CLI

---
+++ nix/home/features/cli/default.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = ["--cmd cd"];
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+    extraOptions = ["-l" "--icons" "--git" "-a"];
+  };
+
+  programs.bat = {enable = true;};
+
+  home.packages = with pkgs; [
+    coreutils
+    fd
+    htop
+    httpie
+    jq
+    procs
+    ripgrep
+    tldr
+    zip
+  ];
+}
