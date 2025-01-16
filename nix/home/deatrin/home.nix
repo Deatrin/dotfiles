7434f3df847043f38d1e69ffe4d4dc8f310053d1
Deatrin <jennexa@gmail.com>
2025-01-15 23:10:34 -0800

    add: nvim config

--- nix/home/deatrin/home.nix
+++ nix/home/deatrin/home.nix
@@ -5,8 +5,11 @@
   config,
   lib,
   pkgs,
+  meta,
   ...
-}: {
+}: let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = lib.mkDefault "deatrin";
