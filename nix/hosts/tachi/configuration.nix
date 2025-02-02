b24eddca36c41db152fc6babf22957b1fff653ee
Deatrin <jennexa@gmail.com>
2025-02-01 19:45:05 -0800

    start building tachi config

--- nix/hosts/tachi/configuration.nix
+++ nix/hosts/tachi/configuration.nix
@@ -13,8 +13,10 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
 
   networking.hostName = "tachi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
