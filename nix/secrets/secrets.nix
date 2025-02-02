2d55cd00aa7999dfa6cb5ad66c58449d52e5506a
Deatrin <jennexa@gmail.com>
2025-02-01 19:38:17 -0800

    add readme stuff

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -2,12 +2,12 @@ let
   # Systems
   razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7TK5R4ssKW9wKCqxh4h4FSfZEUuHC9Ym8PoRBQcun4";
   # tachi = "";
-  # tachivirt = "";
+  tachivirt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZMh5dPobv5smtZTf8677VupkqdXG86tYiS7KWDa47v";
   tycho = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdtJIDcBIYAYkHxvIGF3Sg9avlSjZNU5TKmOZacK4Vk";
 
   # Users
   deatrin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzIb3yvc65BxpM2hjwNhs9viZdYTRm+FCBOLcoVwOzf";
 in {
-  "secret1.age".publicKeys = [razerback tycho deatrin];
-  "deatrin-secrets.age".publicKeys = [razerback tycho deatrin];
+  "secret1.age".publicKeys = [razerback tycho tachivirt deatrin];
+  "deatrin-secrets.age".publicKeys = [razerback tycho tachvirt deatrin];
 }
