0fff203f149962b8cd3f0435d150850bcac51459
Deatrin <jennexa@gmail.com>
2025-02-01 17:37:59 -0800

    Add Tychos config

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -2,11 +2,11 @@ let
   # Systems
   razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7TK5R4ssKW9wKCqxh4h4FSfZEUuHC9Ym8PoRBQcun4";
   # tachi = "";
-  # tycho = "";
+  tycho = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdtJIDcBIYAYkHxvIGF3Sg9avlSjZNU5TKmOZacK4Vk";
 
   # Users
   deatrin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzIb3yvc65BxpM2hjwNhs9viZdYTRm+FCBOLcoVwOzf";
 in {
-  "secret1.age".publicKeys = [razerback deatrin];
-  "deatrin-secrets.age".publicKeys = [razerback deatrin];
+  "secret1.age".publicKeys = [razerback tycho deatrin];
+  "deatrin-secrets.age".publicKeys = [razerback tycho deatrin];
 }
