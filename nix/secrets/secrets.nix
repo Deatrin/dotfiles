a2056d9ee0ca9bbb9309e3e29a8eda5e33b18fae
Deatrin <jennexa@gmail.com>
2025-03-21 21:45:22 -0700

    add nixos anywhere

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   # Systems
   razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7TK5R4ssKW9wKCqxh4h4FSfZEUuHC9Ym8PoRBQcun4";
   # tachi = "";
-  tachivirt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZMh5dPobv5smtZTf8677VupkqdXG86tYiS7KWDa47v";
+  tachivirt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICT5MjnlrTk7bdCBlZdH205XfDyqsT0g9b8I+lyTZd3H";
   tycho = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdtJIDcBIYAYkHxvIGF3Sg9avlSjZNU5TKmOZacK4Vk";
 
   # Users
