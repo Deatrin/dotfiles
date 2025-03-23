66e89b32beff1bff1d95867cc4bfe15f6e8609ee
Deatrin <jennexa@gmail.com>
2025-03-22 21:04:28 -0700

    add tailscale

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # Systems
   razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7TK5R4ssKW9wKCqxh4h4FSfZEUuHC9Ym8PoRBQcun4";
-  # tachi = "";
+  tachi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIElQAUfwtpvKz7XgIhH3nJsMWqpXcvhL0PyenBcyrA6r";
   tachivirt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICT5MjnlrTk7bdCBlZdH205XfDyqsT0g9b8I+lyTZd3H";
   tycho = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdtJIDcBIYAYkHxvIGF3Sg9avlSjZNU5TKmOZacK4Vk";
 
@@ -14,7 +14,7 @@ let
 
   systems = [
     razerback
-    # tachi
+    tachi
     tachivirt
     tycho
   ];
