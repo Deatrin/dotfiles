b9e9b2caf3ed4eb52cdf0ada5cc4bbcaba29f7c1
Deatrin <jennexa@gmail.com>
2025-04-27 10:36:46 -0700

    rekey secrets

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # Systems
   razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7TK5R4ssKW9wKCqxh4h4FSfZEUuHC9Ym8PoRBQcun4";
-  tachi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZwU7D1AaKmtxj70Ujautj6xJEME0ldrmi0tBc9LNtI";
+  tachi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKibh5GTexyqYUlCN5jfhLxxM9AE8irXmkjh+fsbuGTo";
   nauvoo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5lpFFv1P0wcit8dYSN723Um30CUudt6gEykVR/twbB";
   tycho = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIc39gdL4PClqHy8iOajHQWoy15mKujglhWmGMXHNld7";
 
