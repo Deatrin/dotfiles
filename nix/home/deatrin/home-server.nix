afa5d612f6aa197ca23a5dcb994363785853dc14
Deatrin <jennexa@gmail.com>
2025-05-08 10:04:01 -0700

    remove nvim config in place of nvf

--- nix/home/deatrin/home-server.nix
+++ nix/home/deatrin/home-server.nix
@@ -24,8 +24,6 @@ in {
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  xdg.configFile.nvim.source = mkOutOfStoreSymlink "/home/deatrin/dotfiles/.config/nvim";
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
--- nix/home/deatrin/home.nix
+++ nix/home/deatrin/home.nix
@@ -24,7 +24,6 @@ in {
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  #  xdg.configFile.nvim.source = mkOutOfStoreSymlink "/home/deatrin/dotfiles/.config/nvim";
   xdg.configFile.hyprpanel.source = mkOutOfStoreSymlink "/home/deatrin/dotfiles/.config/hyprpanel";
 
   # The home.packages option allows you to install Nix packages into your
