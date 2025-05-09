4705740606389285bf1cf1620d6fb2a9cecdd2fc
Deatrin <jennexa@gmail.com>
2025-05-09 00:30:44 -0700

    Chetzemoka: now to get home manager to build

--- home-manager/common/features/cli/default.nix
+++ home-manager/common/features/cli/default.nix
@@ -1,20 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     # ./atuin.nix
-    # ./bat.nix
-    # ./direnv.nix
-    # ./eza.nix
-    # ./fzf.nix
-    # ./ghostty.nix
-    # ./git.nix
-    # ./gpg.nix
-    # ./nvf.nix
-    # ./oh-my-posh.nix
-    # ./opnix.nix
-    # ./tealdeer.nix
-    # ./tmux.nix
-    # ./zoxide.nix
-    # ./zsh.nix
+    ./bat.nix
+    ./direnv.nix
+    ./eza.nix
+    ./fzf.nix
+    ./ghostty.nix
+    ./git.nix
+    ./gpg.nix
+    ./nvf.nix
+    ./oh-my-posh.nix
+    ./opnix.nix
+    ./tealdeer.nix
+    ./tmux.nix
+    ./zoxide.nix
+    ./zsh.nix
   ];
   home.packages = with pkgs; [
     # _1password # password manager CLI - installing this break op CLI on macbooks
