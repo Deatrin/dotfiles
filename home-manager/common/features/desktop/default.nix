e281e3b5f08a5da85da82ac96b9b478b366adebb
Deatrin <jennexa@gmail.com>
2025-05-10 01:30:03 -0700

    first nix vm built now to test lappys configs

--- home-manager/common/features/desktop/default.nix
+++ home-manager/common/features/desktop/default.nix
@@ -5,4 +5,13 @@
     # ./theme.nix
     ./wayland.nix
   ];
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [thunar-archive-plugin thunar-volman];
+  };
 }
