7747532806e290d07a44e8ce16554afb8008f211
Deatrin <jennexa@gmail.com>
2025-05-11 10:08:04 -0700

    refactor done that was scary fun and worth it

--- hosts/tycho/hardware-configuration.nix
+++ hosts/tycho/hardware-configuration.nix
@@ -12,7 +12,14 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sr_mod"];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "thunderbolt"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+    "sr_mod"
+  ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
