8353704d6063a5aeb9ab7c061b06fc7b0d807ae6
Deatrin <jennexa@gmail.com>
2025-11-28 04:01:38 -0800

    chore: we start navuoo

--- hosts/nauvoo/default.nix
+++ hosts/nauvoo/default.nix
@@ -10,12 +10,12 @@
     inputs.disko.nixosModules.disko
     ./disko-config.nix
     ./hardware-configuration.nix
-    ./secrets.nix
+    # ./secrets.nix
     ../common/nixos
-    ../common/nixos/auto-upgrade.nix
+    # ../common/nixos/auto-upgrade.nix
     ../common/nixos/users/deatrin
     ../common/optional/docker.nix
-    ../common/optional/qemu.nix
+    # ../common/optional/qemu.nix
     ../common/optional/reboot-required.nix
     ../common/optional/vscode-server.nix
   ];
@@ -25,12 +25,12 @@
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
     interfaces.ens18.ipv4.addresses = [
       {
-        address = "10.1.30.10";
+        address = "10.1.10.100";
         prefixLength = 24;
       }
     ];
-    defaultGateway = "10.1.30.1";
-    nameservers = ["10.1.30.1"];
+    defaultGateway = "10.1.10.1";
+    nameservers = ["10.1.10.1"];
   };
 
   # may fix issues with network service failing during a nixos-rebuild
