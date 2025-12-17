83b48eee76b9c936cb81e11f8f1890f0e9528e73
Deatrin <jennexa@gmail.com>
2025-12-17 09:30:01 -0800

    chore: add secrets to nauvoo

--- hosts/nixos/nauvoo/secrets.nix
+++ hosts/nixos/nauvoo/secrets.nix
@@ -1,11 +1,26 @@
+# {
+#   age = {
+#     secrets = {
+#       deatrin-secrets = {
+#         file = ../../../secrets/deatrin-secrets.age;
+#         owner = "deatrin";
+#       };
+#       tailscale-key.file = ../../../secrets/tailscale-key.age;
+#     };
+#   };
+# }
 {
-  age = {
+  services.onepassword-secrets = {
+    enable = true;
+    tokenFile = "/etc/opnix-token";
+    # Ensure deatrin user has access to onepassword-secrets group
+    users = ["deatrin"];
+
     secrets = {
-      deatrin-secrets = {
-        file = ../../../secrets/deatrin-secrets.age;
-        owner = "deatrin";
+      autin = {
+        reference = "op://nix_secrets/atuin/username";
+        mode = "0600";
       };
-      tailscale-key.file = ../../../secrets/tailscale-key.age;
     };
   };
 }
