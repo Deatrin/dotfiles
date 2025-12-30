171afe75d3daae61cb7b51698dd5a836fd6fe290
Deatrin <jennexa@gmail.com>
2025-12-30 08:20:41 -0800

    chore: fix dns

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,6 +1,9 @@
 {
   virtualisation.docker = {
     enable = true;
-    daemon.settings.live-restore = false;
+    daemon.settings = {
+      live-restore = false;
+      dns = ["10.1.30.1" "1.1.1.1"];  # Use local DNS first, then fallback to Cloudflare
+    };
   };
 }
