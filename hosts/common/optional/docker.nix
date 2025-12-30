1ff40e56991434e0cc17a19cd98e38042932b05a
Deatrin <jennexa@gmail.com>
2025-12-30 08:39:42 -0800

    chore: fix dns

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -6,4 +6,7 @@
       dns = ["10.1.30.1" "1.1.1.1"];  # Use local DNS first, then fallback to Cloudflare
     };
   };
+
+  # Allow Docker containers to reach the local DNS server
+  networking.firewall.trustedInterfaces = ["docker0" "br-+"];
 }
