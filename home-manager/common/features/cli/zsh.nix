7890b457314dd9dc73c162167fc8bb21161b88ce
Deatrin <jennexa@gmail.com>
2026-01-05 07:42:12 -0800

    change all themes

--- home-manager/common/features/cli/zsh.nix
+++ home-manager/common/features/cli/zsh.nix
@@ -103,25 +103,28 @@
     };
     plugins = [
       {
+        # Must be loaded first to set up completion system
+        name = "zsh-completions";
+        src = pkgs.zsh-completions;
+        file = "share/zsh-completions/zsh-completions.zsh";
+      }
+      {
+        # Must be loaded after completions, before other plugins
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+      {
         # will source zsh-autosuggestions.plugin.zsh
         name = "zsh-autosuggestions";
         src = pkgs.zsh-autosuggestions;
         file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
       }
       {
-        name = "zsh-completions";
-        src = pkgs.zsh-completions;
-        file = "share/zsh-completions/zsh-completions.zsh";
-      }
-      {
+        # Must be loaded last
         name = "zsh-syntax-highlighting";
         src = pkgs.zsh-syntax-highlighting;
         file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
-      }
-      {
-        name = "fzf-tab";
-        src = pkgs.zsh-fzf-tab;
-        file = "share/fzf-tab/fzf-tab.plugin.zsh";
       }
     ];
   };
