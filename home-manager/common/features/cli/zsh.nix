9d34f34027f32cf0b0fd8212f093a34b267d5c89
Deatrin <jennexa@gmail.com>
2026-01-05 08:29:50 -0800

    fzf fix

--- home-manager/common/features/cli/zsh.nix
+++ home-manager/common/features/cli/zsh.nix
@@ -105,22 +105,22 @@
       {
         # will source zsh-autosuggestions.plugin.zsh
         name = "zsh-autosuggestions";
-        src = pkgs.zsh-autosuggestions;
+        src = pkgs.unstable.zsh-autosuggestions;
         file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
       }
       {
         name = "zsh-completions";
-        src = pkgs.zsh-completions;
+        src = pkgs.unstable.zsh-completions;
         file = "share/zsh-completions/zsh-completions.zsh";
       }
       {
         name = "zsh-syntax-highlighting";
-        src = pkgs.zsh-syntax-highlighting;
+        src = pkgs.unstable.zsh-syntax-highlighting;
         file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
       }
       {
         name = "fzf-tab";
-        src = pkgs.zsh-fzf-tab;
+        src = pkgs.unstable.zsh-fzf-tab;
         file = "share/fzf-tab/fzf-tab.plugin.zsh";
       }
     ];
