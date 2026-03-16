3abe6dffdcc37975de17e7d4eb15d9ac8c607c80
Deatrin <jennexa@gmail.com>
2026-03-16 03:04:42 -0700

    adding logging

--- hosts/common/optional/containers/monitoring/default.nix
+++ hosts/common/optional/containers/monitoring/default.nix
@@ -572,6 +572,7 @@ in {
         image = "quay.io/navidys/prometheus-podman-exporter:latest";
         autoUpdate = "registry";
         networks = [networks.monitoring_network.ref];
+        privileged = true;
         volumes = ["/run/podman/podman.sock:/run/podman/podman.sock"];
         environments = {
           CONTAINER_HOST = "unix:///run/podman/podman.sock";
