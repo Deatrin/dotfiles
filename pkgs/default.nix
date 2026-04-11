4c9d84023fd9920a2fe127303bb022fc6066ec98
Deatrin <jennexa@gmail.com>
2026-04-11 13:42:44 -0700

    custom plex package for latest version

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,10 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-{ pkgs }: {
+{ pkgs }:
+{
   # example = pkgs.callPackage ./example { };
   kubectl-browse-pvc = pkgs.callPackage ./kubectl-browse-pvc.nix {};
+}
+// pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+  plex = pkgs.callPackage ./plex.nix {};
 }
