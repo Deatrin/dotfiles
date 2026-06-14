e25bdc224c8808966e3ea7c535850a5cf70c2fbd
Deatrin <jennexa@gmail.com>
2026-06-14 09:22:22 -0700

    add hdr to main monitor on artemis

--- home-manager/common/features/desktop/hyprland.nix
+++ home-manager/common/features/desktop/hyprland.nix
@@ -20,7 +20,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.enable = false;
+    systemd.enable = true;
     configType = "hyprlang";
     settings = {
       xwayland = {
