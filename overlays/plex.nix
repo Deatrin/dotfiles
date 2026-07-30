4f18accb0b5192d3db43d65b0249e3e1eafbdad4
Deatrin <jennexa@gmail.com>
2026-07-30 02:48:43 -0700

    update plex

--- overlays/plex.nix
+++ overlays/plex.nix
@@ -1,10 +1,10 @@
 final: prev:
 prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
   plexRaw = prev.plexRaw.overrideAttrs (_old: rec {
-    version = "1.43.2.10687-563d026ea";
+    version = "1.43.3.10828-00f62d37d";
     src = prev.fetchurl {
       url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-      sha256 = "sha256-dgkj0Uny/d0DnExgYWjxfl2cFsiattlGzb7Guzmtro4=";
+      sha256 = "sha256-ieU0/7Vlrs2tsR1QhD2Cyk/pia4MfmAugx0Ec6Ook20=";
     };
   });
 }
