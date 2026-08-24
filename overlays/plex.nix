ae2dff195e957c0a249ad2e3c62188cd5b24338e
Deatrin <jennexa@gmail.com>
2026-08-23 22:56:20 -0700

    bump plex

--- overlays/plex.nix
+++ overlays/plex.nix
@@ -1,10 +1,10 @@
 final: prev:
 prev.lib.optionalAttrs prev.stdenv.hostPlatform.isLinux {
   plexRaw = prev.plexRaw.overrideAttrs (_old: rec {
-    version = "1.43.3.10828-00f62d37d";
+    version = "1.43.3.10896-cb3ebc72d";
     src = prev.fetchurl {
       url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-      sha256 = "sha256-ieU0/7Vlrs2tsR1QhD2Cyk/pia4MfmAugx0Ec6Ook20=";
+      sha256 = "sha256-qgnyZt3PQI4Qz3ulYbbkVObhCbqUFjlraWW9THnzcUk=";
     };
   });
 }
